7472b09b0139da8cfd17ec4653f0d84ab3b87958
Lars <kleijnen.lars@gmail.com>
2025-05-22 21:23:07 +0200

    Toevoegen aan winkelmandje

--- KE03_INTDEV_SE_1_Base/wwwroot/js/site.js
+++ KE03_INTDEV_SE_1_Base/wwwroot/js/site.js
@@ -1,4 +1,48 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//function addToCart(productId, name, price) {
+//    let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-// Write your JavaScript code.
+//    // Voeg product toe
+//    cart.push({ id: productId, name: name, price: price });
+//    localStorage.setItem('cart', JSON.stringify(cart));
+
+//    // Tel aantal en totaalbedrag op
+//    const totalItems = cart.length;
+
+
+//    // Toon gegevens in modal
+//    const body = document.getElementById('cartModalBody');
+//    body.innerText = `Je hebt nu ${totalItems} product${totalItems === 1 ? '' : 'en'} in je winkelmandje.`;
+
+//    // Toon modal
+//    const modal = new bootstrap.Modal(document.getElementById('cartModal'));
+//    modal.show();
+//}
+
+function addToCart(productId, name, price) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    // Zoek of het product al in de winkelmand zit
+    const existingProduct = cart.find(p => p.id === productId);
+
+    if (existingProduct) {
+        // Verhoog de hoeveelheid
+        existingProduct.quantity += 1;
+    } else {
+        // Voeg nieuw product toe met quantity 1
+        cart.push({ id: productId, quantity: 1 });
+    }
+
+    // Sla het winkelmandje weer op
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    // Bereken totaal aantal producten (optellen van quantity)
+    const totalItems = cart.reduce((sum, p) => sum + p.quantity, 0);
+
+    // Update de modal tekst
+    const body = document.getElementById('cartModalBody');
+    body.innerText = `Je hebt nu ${totalItems} product${totalItems === 1 ? '' : 'en'} in je winkelmandje.`;
+
+    // Toon de modal
+    const modal = new bootstrap.Modal(document.getElementById('cartModal'));
+    modal.show();
+}
